5590c2f23509b7181b9cfe2749090649dd26c50d
MisterArun <78886361+MisterArun@users.noreply.github.com>
2023-04-17 16:05:09 -0400

    Fixed movement

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
+    //public Transform bulletSpawnPoint;
+    //public GameObject bulletPrefab;
+
     public float moveSpeed = 1f;
+    // public float bulletSpeed = 1f;
     public float rotateSpeed = 12f;
 
-    private void FixedUpdate()
+    private void Update()
     {
-        float moveVector = Input.GetAxis("Vertical");
-        float rotateVector = Input.GetAxis("Horizontal");
+        float moveVector = Input.GetAxisRaw("Vertical");
+        float rotateVector = Input.GetAxisRaw("Horizontal");
 
-        this.transform.Translate(0f, moveVector * moveSpeed * Time.deltaTime, 0f);
-        this.transform.Rotate(0f, 0f, -rotateVector * (rotateSpeed * 10) * Time.deltaTime);
+        this.transform.Translate(0f, - moveVector * moveSpeed * Time.deltaTime, 0f);
+        this.transform.Rotate(0f, 0f, - rotateVector * (rotateSpeed * 10) * Time.deltaTime);
+        // Shooting();
     }
+
+    /*
+    void Shooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        }
+    }
+   */
 }
