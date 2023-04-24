c1a931a857d37cdffcc998fbfa5af2afe68c4609
MisterArun <78886361+MisterArun@users.noreply.github.com>
2023-04-24 10:38:29 -0400

    2 tanks controller

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -4,31 +4,48 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
-    //public Transform bulletSpawnPoint;
-    //public GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawnPoint;
+    [SerializeField] GameObject bulletPrefab;
+
+    float nextShootTime = 0f;
 
     public float moveSpeed = 1f;
-    // public float bulletSpeed = 1f;
+    public float bulletSpeed = 1f;
     public float rotateSpeed = 12f;
 
-    private void Update()
+    void Update()
     {
-        float moveVector = Input.GetAxisRaw("Vertical");
-        float rotateVector = Input.GetAxisRaw("Horizontal");
+        bool player1FireKey = Input.GetKeyDown(KeyCode.Space);
+        bool player2FireKey = Input.GetKeyDown(KeyCode.M);
 
-        this.transform.Translate(0f, - moveVector * moveSpeed * Time.deltaTime, 0f);
-        this.transform.Rotate(0f, 0f, - rotateVector * (rotateSpeed * 10) * Time.deltaTime);
-        // Shooting();
+        if (transform.tag == "Player1")
+        {
+            float moveVector = Input.GetAxisRaw("Vertical");
+            float rotateVector = Input.GetAxisRaw("Horizontal");
+
+            this.transform.Translate(0f, - moveVector * moveSpeed * Time.deltaTime, 0f);
+            this.transform.Rotate(0f, 0f, - rotateVector * (rotateSpeed * 10) * Time.deltaTime);
+            Shooting(player1FireKey);
+        }else if(transform.tag == "Player2")
+        {
+            float moveVector = Input.GetAxisRaw("Vertical2");
+            float rotateVector = Input.GetAxisRaw("Horizontal2");
+
+            this.transform.Translate(0f, -moveVector * moveSpeed * Time.deltaTime, 0f);
+            this.transform.Rotate(0f, 0f, -rotateVector * (rotateSpeed * 10) * Time.deltaTime);
+            Shooting(player2FireKey);
+        } 
     }
 
-    /*
-    void Shooting()
+    void Shooting(bool key)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (key && Time.time > nextShootTime) 
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            nextShootTime = Time.time + 0.7f;
+            GameObject newBullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, Quaternion.identity);
+            Rigidbody2D bulletRB = newBullet.gameObject.GetComponent<Rigidbody2D>();
+            bulletRB.velocity = bulletSpawnPoint.transform.position * bulletSpeed;
         }
     }
-   */
+   
 }
