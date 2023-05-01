6af32896394e425ba10d66a2ea547f72ef7a0f06
Munir Khaliqyar <munir.khaliqyar@gmail.com>
2023-05-01 11:07:50 -0400

    edited the TrapController

--- Assets/Scripts/TrapController.cs
+++ Assets/Scripts/TrapController.cs
@@ -12,25 +12,35 @@ public class TrapController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animator = gameObject.GetComponent<Animator>();   
+        animator = gameObject.GetComponent<Animator>();
     }
 
     private void Update()
     {
+        Vector2 player1Pos = GameManager.player1.transform.position;
+        Vector2 player2Pos = GameManager.player2.transform.position;
+        
         float maxHeight = Random.Range(-4.5f, 4.5f);
         float maxWeith = Random.Range(-7.5f, 10f);
-        Vector2 position = new Vector2(maxWeith, maxHeight);
-        if(trapAmount < maxTrapAmount)
-        {
-            trapAmount++;
-            GameObject newTrap = Instantiate(trap, position, Quaternion.Euler(0,0,180f));
-            newTrap.SetActive(true);
-        }
+        Vector2 trapPosition = new Vector2(maxWeith, maxHeight);
+
+        // Get the distance between object1 and object2
+        float player1Distance = Vector2.Distance(player1Pos, trapPosition);
+        float player2Distance = Vector2.Distance(player2Pos, trapPosition);
+
+        if (trapAmount < maxTrapAmount)
+        
+            if (player1Distance > 3 && player2Distance > 3)
+            {
+                trapAmount++;
+                GameObject newTrap = Instantiate(trap, trapPosition, Quaternion.Euler(0,0,180f));
+                newTrap.SetActive(true);
+            }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2"))
         {
             animator.SetBool("Explosion", true);
             trapAmount--;
