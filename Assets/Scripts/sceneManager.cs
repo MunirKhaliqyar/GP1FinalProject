ce14930e72a558955109c72f3dfdb391a27f26a3
Munir Khaliqyar <munir.khaliqyar@gmail.com>
2023-05-06 02:00:08 -0400

    Modified sceneManager.cs to get the UserNamePrefs from both users

--- Assets/Scripts/sceneManager.cs
+++ Assets/Scripts/sceneManager.cs
@@ -1,12 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class sceneManager : MonoBehaviour
 {
+    private Button startButton;
+    private bool continueGame = false;
+
+    private void Start()
+    {
+        startButton = GetComponent<Button>();
+        startButton.enabled = false;
+    }
+
+    private void Update()
+    {
+        string rightUserName = PlayerPrefs.GetString("rightUserName");
+        string leftUserName = PlayerPrefs.GetString("leftUserName");
+        string rightTankColor = PlayerPrefs.GetString("rightTankColor");
+        string leftTankColor = PlayerPrefs.GetString("leftTankColor");
+        string mapChoice = PlayerPrefs.GetString("mapChoice");
+
+        if (rightUserName == "" || leftUserName == "" || rightTankColor == leftTankColor || mapChoice == "")
+        {
+            continueGame = false;
+            startButton.enabled = false;
+        }
+        else
+        {
+            continueGame = true;
+            startButton.enabled = true;
+
+        }
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (continueGame == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
